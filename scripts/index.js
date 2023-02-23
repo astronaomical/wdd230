c72d867c545a767922a824ec0b92c5f7b4080961
astronaomical <astronaomical@gmail.com>
2023-02-22 19:34:23 -0700

    Added JavaScript

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,7 @@
 year = document.querySelector("#year");
 const currentYear = new Date().getFullYear();
 year.textContent= currentYear;
+
+let lastModified = new Date(document.lastModified);
+
+document.getElementById("lastmodified").textContent = lastModified;
